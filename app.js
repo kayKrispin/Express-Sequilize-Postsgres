786b8d1c11f31b10468fb735977fea0d19c60af0
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2021-03-02 00:44:45 +0200

    remove service

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ client.connect(function(err) {
       return console.error('error running query', err);
     }
     console.log(result.rows[0].theTime);
+    console.log("hi connected")
     // >> output: 2018-08-23T14:02:57.117Z
     client.end();
   });
